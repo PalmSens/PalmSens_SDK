53450b3848bed13a88dd930f303ea18a86ee9d66
Stef Smeets <stef@palmsens.com>
2025-07-14 09:44:13 +0200

    Get rid of convert_to_curves function

--- python/pspython/pspydata.py
+++ python/pspython/pspydata.py
@@ -1,11 +1,9 @@
-from .data.convert import convert_to_curves
 from .data.curve import Curve
 from .data.fit_result import EISFitResult
 from .data.measurement import Measurement
 from .data.peak import Peak
 
 __all__ = [
-    'convert_to_curves',
     'Curve',
     'EISFitResult',
     'Measurement',
