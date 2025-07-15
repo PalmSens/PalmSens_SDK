eb886feac37a3aa1004fdd5ea00af26a33fd9bf9
Stef Smeets <stef@palmsens.com>
2025-07-15 16:34:08 +0200

    Add data array test

--- python/pspython/data/data_array.py
+++ python/pspython/data/data_array.py
@@ -14,6 +14,9 @@ class DataArray(Sequence):
 
     def __getitem__(self, index):
         if isinstance(index, int):
+            if index >= len(self) or index < -len(self):
+                raise IndexError('list index out of range')
+            index = index % len(self)
             return self.dotnet_data_array[index].Value
 
         return self.to_list()[index]
@@ -21,8 +24,17 @@ class DataArray(Sequence):
     def __len__(self):
         return len(self.dotnet_data_array)
 
+    def min(self) -> float:
+        """Return min value."""
+        return self.dotnet_data_array.MinValue
+
+    def max(self) -> float:
+        """Return max value."""
+        return self.dotnet_data_array.MaxValue
+
     @property
     def name(self):
+        """Name of the array."""
         return self.dotnet_data_array.Description
 
     def to_numpy(self) -> np.ndarray:
@@ -35,6 +47,7 @@ class DataArray(Sequence):
 
     @property
     def type(self) -> ArrayType:
+        """ArrayType enum."""
         return ArrayType(self.dotnet_data_array.ArrayType)
 
     @property
