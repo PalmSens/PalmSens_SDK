adca3d03ecef0f1c5a64b28239411cfebb591814
Stef Smeets <stef@palmsens.com>
2025-07-17 16:56:16 +0200

    Refactor CV function to dataclass

--- python/pspython/methods/cyclic_voltammetry.py
+++ python/pspython/methods/cyclic_voltammetry.py
@@ -1,3 +1,7 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+import PalmSens
 from PalmSens.Techniques import CyclicVoltammetry as PSCyclicVoltammetry
 
 from ._shared import (
@@ -15,13 +19,10 @@ from ._shared import (
     set_trigger_at_measurement_settings,
     set_versus_ocp,
 )
-from .potential_method import PotentialMethod
 
 
-class CyclicVoltammetry(PotentialMethod): ...
-
-
-def cyclic_voltammetry(**kwargs):
+@dataclass
+class CyclicVoltammetry:
     """Create a cyclic voltammetry method object.
 
     :Keyword Arguments:
@@ -114,163 +115,194 @@ def cyclic_voltammetry(**kwargs):
     * use_hardware_sync : bool --
         Use hardware synchronization with other channels/instruments (default: False)
     """
-    cyclic_voltammetry = PSCyclicVoltammetry()
 
-    # (auto)ranging
-    current_range_max = kwargs.get('current_range_max', get_current_range(8))
-    current_range_min = kwargs.get('current_range_min', get_current_range(4))
-    current_range_start = kwargs.get('current_range_start', get_current_range(6))
-    set_autoranging_current(
-        cyclic_voltammetry, current_range_max, current_range_min, current_range_start
-    )
+    # autoranging
+    current_range_max: int = get_current_range(8)
+    current_range_min: int = get_current_range(4)
+    current_range_start: int = get_current_range(6)
 
     # pretreatment
-    deposition_potential = kwargs.get('deposition_potential', 0.0)
-    deposition_time = kwargs.get('deposition_time', 0.0)
-    conditioning_potential = kwargs.get('conditioning_potential', 0.0)
-    conditioning_time = kwargs.get('conditioning_time', 0.0)
-    set_pretreatment(
-        cyclic_voltammetry,
-        deposition_potential,
-        deposition_time,
-        conditioning_potential,
-        conditioning_time,
-    )
+    deposition_potential: float = 0.0
+    deposition_time: float = 0.0
+    conditioning_potential: float = 0.0
+    conditioning_time: float = 0.0
+    equilibration_time: float = 0.0  # Time (s)
 
     # cyclic voltammetry settings
-    equilibration_time = kwargs.get('equilibration_time', 0.0)  # Time (s)
-    begin_potential = kwargs.get('begin_potential', -0.5)  # potential (V)
-    vertex1_potential = kwargs.get('vertex1_potential', 0.5)  # potential (V)
-    vertex2_potential = kwargs.get('vertex2_potential', -0.5)  # potential (V)
-    step_potential = kwargs.get('step_potential', 0.1)  # potential (V)
-    scanrate = kwargs.get('scanrate', 1.0)  # potential/time (V/s)
-    n_scans = kwargs.get('n_scans', 1)  # number of cycles (default: 1)
-    cyclic_voltammetry.EquilibrationTime = equilibration_time
-    cyclic_voltammetry.BeginPotential = begin_potential
-    cyclic_voltammetry.Vtx1Potential = vertex1_potential
-    cyclic_voltammetry.Vtx2Potential = vertex2_potential
-    cyclic_voltammetry.StepPotential = step_potential
-    cyclic_voltammetry.Scanrate = scanrate
-    cyclic_voltammetry.nScans = n_scans
+    begin_potential: float = -0.5  # potential (V)
+    vertex1_potential: float = 0.5  # potential (V)
+    vertex2_potential: float = -0.5  # potential (V)
+    step_potential: float = 0.1  # potential (V)
+    scanrate: float = 1.0  # potential/time (V/s)
+    n_scans: float = 1  # number of cycles (default: 1)
 
     # advanced settings
     # versus OCP settings
-    versus_ocp_mode = kwargs.get(
-        'versus_ocp_mode', 0
-    )  # 0 = disable versus OCP, 1 = vertex 1 potential, 2 = vertex 2 potential, 3 = vertex 1 & 2 potential, 4 = begin potential, 5 = begin & vertex 1 potential, 6 = begin & vertex 2 potential, 7 = begin & vertex 1 & 2 potential
-    versus_ocp_max_ocp_time = kwargs.get('versus_ocp_max_ocp_time', 20)  # Time (s)
-    versus_ocp_stability_criterion = kwargs.get(
-        'versus_ocp_stability_criterion', 0
-    )  # 0 = no stability criterion, > 0 is stability threshold potential/time (mV/s)
-    set_versus_ocp(
-        cyclic_voltammetry,
-        versus_ocp_mode,
-        versus_ocp_max_ocp_time,
-        versus_ocp_stability_criterion,
-    )
+    # 0 = disable versus OCP, 1 = vertex 1 potential, 2 = vertex 2 potential, 3 = vertex 1 & 2 potential, 4 = begin potential, 5 = begin & vertex 1 potential, 6 = begin & vertex 2 potential, 7 = begin & vertex 1 & 2 potential
+    versus_ocp_mode: int = 0
+    versus_ocp_max_ocp_time: int = 20  # Time (s)
+    # 0 = no stability criterion, > 0 is stability threshold potential/time (mV/s)
+    versus_ocp_stability_criterion: int = 0
 
     # bipot settings
-    enable_bipot_current = kwargs.get('enable_bipot_current', False)
-    bipot_mode = kwargs.get('bipot_mode', 0)  # 0 = constant, 1 = offset
-    bipot_potential = kwargs.get('bipot_potential', 0.0)  # in V
-    bipot_current_range_max = kwargs.get('bipot_current_range_max', get_current_range(8))
-    bipot_current_range_min = kwargs.get('bipot_current_range_min', get_current_range(4))
-    bipot_current_range_start = kwargs.get('bipot_current_range_start', get_current_range(6))
-    set_bipot_settings(
-        cyclic_voltammetry,
-        bipot_mode,
-        bipot_potential,
-        bipot_current_range_max,
-        bipot_current_range_min,
-        bipot_current_range_start,
-    )
+    enable_bipot_current: bool = False
+    bipot_mode: int = 0  # 0 = constant, 1 = offset
+    bipot_potential: float = 0.0  # in V
+    bipot_current_range_max: int = get_current_range(8)
+    bipot_current_range_min: int = get_current_range(4)
+    bipot_current_range_start: int = get_current_range(6)
 
     # record extra value settings
-    record_auxiliary_input = kwargs.get('record_auxiliary_input', False)
-    record_cell_potential = kwargs.get('record_cell_potential', False)
-    record_we_potential = kwargs.get('record_we_potential', False)
-    set_extra_value_mask(
-        cyclic_voltammetry,
-        enable_bipot_current=enable_bipot_current,
-        record_auxiliary_input=record_auxiliary_input,
-        record_cell_potential=record_cell_potential,
-        record_we_potential=record_we_potential,
-    )
+    record_auxiliary_input: bool = False
+    record_cell_potential: bool = False
+    record_we_potential: bool = False
 
     # post measurement settings
-    cell_on_after_measurement = kwargs.get('cell_on_after_measurement', False)
-    cell_on_after_measurement_potential = kwargs.get(
-        'cell_on_after_measurement_potential', 0.0
-    )  # in V
-    set_post_measurement_settings(
-        cyclic_voltammetry, cell_on_after_measurement, cell_on_after_measurement_potential
-    )
+    cell_on_after_measurement: bool = False
+    cell_on_after_measurement_potential: float = 0.0  # in V
 
     # limit settings
-    use_limit_current_max = kwargs.get('use_limit_current_max', False)
-    limit_current_max = kwargs.get('limit_current_max', 0.0)  # in µA
-    use_limit_current_min = kwargs.get('use_limit_current_min', False)
-    limit_current_min = kwargs.get('limit_current_min', 0.0)  # in µA
-    set_limit_settings(
-        cyclic_voltammetry,
-        use_limit_current_max,
-        limit_current_max,
-        use_limit_current_min,
-        limit_current_min,
-    )
+    use_limit_current_max: bool = False
+    limit_current_max: float = 0.0  # in µA
+    use_limit_current_min: bool = False
+    limit_current_min: float = 0.0  # in µA
 
     # iR compensation settings
-    use_ir_compensation = kwargs.get('use_ir_compensation', False)
-    ir_compensation = kwargs.get('ir_compensation', 0.0)  # IR compensation in Ω
-    set_ir_drop_compensation(cyclic_voltammetry, use_ir_compensation, ir_compensation)
+    use_ir_compensation: bool = False
+    ir_compensation: float = 0.0  # IR compensation in Ω
 
     # set trigger settings
-    trigger_at_equilibration = kwargs.get('trigger_at_equilibration', False)
-    trigger_at_equilibration_lines = kwargs.get(
-        'trigger_at_equilibration_lines', [False, False, False, False]
-    )  # d0 high, d1 high, d2 high, d3 high
-    trigger_at_measurement = kwargs.get('trigger_at_measurement', False)
-    trigger_at_measurement_lines = kwargs.get(
-        'trigger_at_measurement_lines', [False, False, False, False]
-    )  # d0 high, d1 high, d2 high, d3 high
-    set_trigger_at_equilibration_settings(
-        cyclic_voltammetry, trigger_at_equilibration, trigger_at_equilibration_lines
-    )
-    set_trigger_at_measurement_settings(
-        cyclic_voltammetry, trigger_at_measurement, trigger_at_measurement_lines
-    )
+    trigger_at_equilibration: bool = False
+    # d0 high, d1 high, d2 high, d3 high
+    trigger_at_equilibration_lines: tuple[bool, bool, bool, bool] = (False, False, False, False)
+    trigger_at_measurement: bool = False
+    # d0 high, d1 high, d2 high, d3 high
+    trigger_at_measurement_lines: tuple[bool, bool, bool, bool] = (False, False, False, False)
 
     # set filter settings
-    dc_mains_filter = kwargs.get(
-        'dc_mains_filter', 50
-    )  # set to 50 Hz or 60 Hz (50 Hz is default)
-    default_curve_post_processing_filter = kwargs.get(
-        'default_curve_post_processing_filter', 0
-    )  # -1 = no filter, 0 = spike rejection, 1 = spike rejection + Savitsky-golay window 5, 2 = spike rejection + Savitsky-golay window 9, 3 = spike rejection + Savitsky-golay window 15, 4 = spike rejection + Savitsky-golay window 25
-    set_filter_settings(
-        cyclic_voltammetry, dc_mains_filter, default_curve_post_processing_filter
-    )
+    # set to 50 Hz or 60 Hz (50 Hz is default)
+    dc_mains_filter: int = 50
+    # -1 = no filter, 0 = spike rejection, 1 = spike rejection + Savitsky-golay window 5, 2 = spike rejection + Savitsky-golay window 9, 3 = spike rejection + Savitsky-golay window 15, 4 = spike rejection + Savitsky-golay window 25
+    default_curve_post_processing_filter: int = 0
 
     # multiplexer settings
-    set_mux_mode = kwargs.get(
-        'set_mux_mode', -1
-    )  # -1 = disable, 0 = sequentially, 1 = alternatingly
-    set_mux_channels = kwargs.get(
-        'set_mux_channels', [False, False, False, False, False, False, False, False]
-    )  # a list of bools for each channel (channel 1, channel 2, ..., channel 128). In consequtive mode all selections are valid, in alternating mode the first channel must be selected and all other channels should be consequtive i.e. (channel 1, channel 2, channel 3 and so on).
-    set_mux8r2_settings = kwargs.get(
-        'set_mux8r2_settings', None
-    )  # Initialize the settings for the MUX8R2 multiplexer, PalmSens.Method.MuxSettings, use get_mux8r2_settings() to create the settings
-    set_multiplexer_settings(
-        cyclic_voltammetry, set_mux_mode, set_mux_channels, set_mux8r2_settings
+    # -1 = disable, 0 = sequentially, 1 = alternatingly
+    set_mux_mode: int = -1
+    # a list of bools for each channel (channel 1, channel 2, ..., channel 128). In consequtive mode all selections are valid, in alternating mode the first channel must be selected and all other channels should be consequtive i.e. (channel 1, channel 2, channel 3 and so on).
+    set_mux_channels: list[bool] = field(
+        default_factory=lambda: [False, False, False, False, False, False, False, False]
     )
+    # Initialize the settings for the MUX8R2 multiplexer, PalmSens.Method.MuxSettings, use get_mux8r2_settings() to create the settings
+    set_mux8r2_settings: Optional[PalmSens.Method.MuxSettings] = None
 
     # internal storage
-    save_on_internal_storage = kwargs.get('save_on_internal_storage', False)
-    cyclic_voltammetry.SaveOnDevice = save_on_internal_storage
+    save_on_internal_storage: bool = False
 
     # use hardware synchronization with other channels/instruments
-    use_hardware_sync = kwargs.get('use_hardware_sync', False)
-    cyclic_voltammetry.UseHWSync = use_hardware_sync
+    use_hardware_sync: bool = False
 
-    return cyclic_voltammetry
+    def to_dotnet_method(self):
+        obj = PSCyclicVoltammetry()
+
+        set_autoranging_current(
+            obj,
+            current_range_max=self.current_range_max,
+            current_range_min=self.current_range_min,
+            current_range_start=self.current_range_start,
+        )
+
+        set_pretreatment(
+            obj,
+            deposition_potential=self.deposition_potential,
+            deposition_time=self.deposition_time,
+            conditioning_potential=self.conditioning_potential,
+            conditioning_time=self.conditioning_time,
+        )
+        obj.EquilibrationTime = self.equilibration_time
+
+        # cyclic voltammetry settings
+        obj.BeginPotential = self.begin_potential
+        obj.Vtx1Potential = self.vertex1_potential
+        obj.Vtx2Potential = self.vertex2_potential
+        obj.StepPotential = self.step_potential
+        obj.Scanrate = self.scanrate
+        obj.nScans = self.n_scans
+
+        set_versus_ocp(
+            obj,
+            versus_ocp_mode=self.versus_ocp_mode,
+            versus_ocp_max_ocp_time=self.versus_ocp_max_ocp_time,
+            versus_ocp_stability_criterion=self.versus_ocp_stability_criterion,
+        )
+
+        set_bipot_settings(
+            obj,
+            bipot_mode=self.bipot_mode,
+            bipot_potential=self.bipot_potential,
+            bipot_current_range_max=self.bipot_current_range_max,
+            bipot_current_range_min=self.bipot_current_range_min,
+            bipot_current_range_start=self.bipot_current_range_start,
+        )
+
+        set_extra_value_mask(
+            obj,
+            enable_bipot_current=self.enable_bipot_current,
+            record_auxiliary_input=self.record_auxiliary_input,
+            record_cell_potential=self.record_cell_potential,
+            record_we_potential=self.record_we_potential,
+        )
+
+        set_post_measurement_settings(
+            obj,
+            cell_on_after_measurement=self.cell_on_after_measurement,
+            cell_on_after_measurement_potential=self.cell_on_after_measurement_potential,
+        )
+
+        set_limit_settings(
+            obj,
+            use_limit_max=self.use_limit_current_max,
+            limit_max=self.limit_current_max,
+            use_limit_min=self.use_limit_current_min,
+            limit_min=self.limit_current_min,
+        )
+
+        set_ir_drop_compensation(
+            obj,
+            use_ir_compensation=self.use_ir_compensation,
+            ir_compensation=self.ir_compensation,
+        )
+
+        set_trigger_at_equilibration_settings(
+            obj,
+            trigger_at_equilibration=self.trigger_at_equilibration,
+            trigger_at_equilibration_lines=self.trigger_at_equilibration_lines,
+        )
+        set_trigger_at_measurement_settings(
+            obj,
+            trigger_at_measurement=self.trigger_at_measurement,
+            trigger_at_measurement_lines=self.trigger_at_measurement_lines,
+        )
+
+        set_filter_settings(
+            obj,
+            dc_mains_filter=self.dc_mains_filter,
+            default_curve_post_processing_filter=self.default_curve_post_processing_filter,
+        )
+
+        set_multiplexer_settings(
+            obj,
+            set_mux_mode=self.set_mux_mode,
+            set_mux_channels=self.set_mux_channels,
+            set_mux8r2_settings=self.set_mux8r2_settings,
+        )
+
+        obj.SaveOnDevice = self.save_on_internal_storage
+        obj.UseHWSync = self.use_hardware_sync
+
+        return obj
+
+
+def cyclic_voltammetry(**kwargs):
+    """Alias for CyclicVoltammetry for backwards compatibility"""
+    cyclic_voltammetry = CyclicVoltammetry(**kwargs)
+    return cyclic_voltammetry.to_dotnet_method()
--- python/tests/instruments_test.py
+++ python/tests/instruments_test.py
@@ -2,8 +2,10 @@ import logging
 
 import pytest
 
-from pspython import pspyinstruments, pspymethods
+from pspython import pspyinstruments
 from pspython.data.measurement import Measurement
+from pspython.methods._shared import get_current_range
+from pspython.methods.cyclic_voltammetry import CyclicVoltammetry, cyclic_voltammetry
 
 logger = logging.getLogger(__name__)
 
@@ -13,7 +15,7 @@ def manager():
     mgr = pspyinstruments.InstrumentManager()
 
     available_instruments = pspyinstruments.discover_instruments()
-    logger.info('Connecting to %s' % available_instruments[0].name)
+    logger.warning('Connecting to %s' % available_instruments[0].name)
     success = mgr.connect(available_instruments[0])
     assert success
 
@@ -41,11 +43,11 @@ def test_read_potential(manager):
     assert isinstance(val, float)
 
 
-def test_cv(manager):
-    method = pspymethods.cyclic_voltammetry(
-        current_range_max=pspymethods.get_current_range(30),  # 1A range
-        current_range_min=pspymethods.get_current_range(4),  # 1µA range
-        current_range_start=pspymethods.get_current_range(8),  # 1mA range
+def test_cv_old(manager):
+    method = cyclic_voltammetry(
+        current_range_max=get_current_range(30),  # 1A range
+        current_range_min=get_current_range(4),  # 1µA range
+        current_range_start=get_current_range(8),  # 1mA range
         equilibration_time=0,  # seconds
         begin_potential=-1,  # V
         vertex1_potential=-1,  # V
@@ -56,6 +58,32 @@ def test_cv(manager):
     )
 
     measurement = manager.measure(method)
+
+    assert measurement
+    assert isinstance(measurement, Measurement)
+    assert measurement.method.dotnet_method.nScans == 2
+
+    dataset = measurement.dataset
+    assert len(dataset) == 7
+    assert dataset.array_names == {'scan1', 'scan2', 'time'}
+    assert dataset.array_quantities == {'Charge', 'Current', 'Potential', 'Time'}
+
+
+def test_cv_new(manager):
+    method = CyclicVoltammetry(
+        current_range_max=get_current_range(30),  # 1A range
+        current_range_min=get_current_range(4),  # 1µA range
+        current_range_start=get_current_range(8),  # 1mA range
+        equilibration_time=0,  # seconds
+        begin_potential=-1,  # V
+        vertex1_potential=-1,  # V
+        vertex2_potential=1,  # V
+        step_potential=0.25,  # V
+        scanrate=20,  # V/s
+        n_scans=2,  # number of scans
+    )
+
+    measurement = manager.measure(method.to_dotnet_method())
 
     assert measurement
     assert isinstance(measurement, Measurement)
