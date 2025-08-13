e45143d5c8872f237bfdb12337f9bfdee9e57295
Stef Smeets <stef@palmsens.com>
2025-08-13 09:21:30 +0200

    Make mypy happy

--- python/pspython/_lib/mono.py
+++ python/pspython/_lib/mono.py
@@ -3,19 +3,19 @@ from __future__ import annotations
 import atexit
 import platform
 from importlib.resources import files
+from pathlib import Path
 
 from pythonnet import load, unload
 
 PSSDK_DIR = files('pspython._pssdk.mono')
-
 
 # runtime must be imported before clr is loaded
 load('coreclr', runtime_config=str(PSSDK_DIR / 'runtimeconfig.json'))
 
 import clr  # noqa: E402
 
-core_dll = PSSDK_DIR / 'PalmSens.Core'
-core_linux_dll = PSSDK_DIR / 'PalmSens.Core.Linux'
+core_dll = PSSDK_DIR / 'PalmSens.Core.dll'
+core_linux_dll = PSSDK_DIR / 'PalmSens.Core.Linux.dll'
 
 RUNTIME_DIR = files('pspython._runtimes')
 
@@ -34,15 +34,19 @@ PLATFORMS = {
 
 plat = PLATFORMS[system, machine]
 
-ioports_dll = RUNTIME_DIR / plat / 'native' / 'System.IO.Ports'
+ioports_dll = RUNTIME_DIR / plat / 'native' / 'System.IO.Ports.dll'
+
+assert isinstance(core_dll, Path)
+assert isinstance(core_linux_dll, Path)
+assert isinstance(ioports_dll, Path)
 
 # This dll contains the classes in which the data is stored
-clr.AddReference(str(core_dll))
+clr.AddReference(str(core_dll.with_suffix('')))
 
 # This dll is used to load your session file
-clr.AddReference(str(core_linux_dll))
+clr.AddReference(str(core_linux_dll.with_suffix('')))
 
-clr.AddReference(str(ioports_dll))
+clr.AddReference(str(ioports_dll.with_suffix('')))
 
 clr.AddReference('System')
 
@@ -51,6 +55,6 @@ from System import Diagnostics  # noqa: E402
 
 CoreDependencies.Init()
 
-sdk_version = Diagnostics.FileVersionInfo.GetVersionInfo(str(core_dll) + '.dll').ProductVersion
+sdk_version = Diagnostics.FileVersionInfo.GetVersionInfo(str(core_dll)).ProductVersion
 
 atexit.register(unload)
