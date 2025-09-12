16a33c3e49e4edeaa548b3d69dc7b502d6747e30
Stef Smeets <stef@palmsens.com>
2025-09-12 16:42:58 +0200

    Implement HW sync, custom function, callback on instrument pool

--- python/examples/multichannel_measurement_2.py
+++ python/examples/multichannel_measurement_2.py
@@ -1,6 +1,23 @@
 import asyncio
 
 import pypalmsens
+import csv
+
+from pypalmsens._instruments.instrument_manager_async import InstrumentManagerAsync
+from pypalmsens._methods.techniques import MethodSettings
+
+
+def new_data_callback(new_data):
+    for point in new_data:
+        print(point)
+
+
+def stream_to_csv_callback(csv_writer):
+    def stream_to_csv_callback_channel(new_data):
+        for point in new_data:
+            csv_writer.writerow([point['index'], point['x'], point['y']])
+
+    return stream_to_csv_callback_channel
 
 
 async def main():
@@ -18,6 +35,42 @@ async def main():
         results = await asyncio.gather(*tasks)
 
     print(results)
+
+    # run multichannel experiment with callback
+    async with pypalmsens.InstrumentPoolAsync(
+        instruments,
+        callback=new_data_callback,
+    ) as pool:
+        tasks = await pool.measure(method)
+        results = await asyncio.gather(*tasks)
+
+    # run multichannel experiment with custom task
+    async def my_task(manager: InstrumentManagerAsync, method: MethodSettings):
+        serial = await manager.get_instrument_serial()
+        csv_file = open(f'{serial}.csv', 'w', newline='')
+        csv_writer = csv.writer(csv_file, delimiter=' ')
+        manager.callback = stream_to_csv_callback(csv_writer)
+        measurement = await manager.measure(method)
+        csv_file.close()
+        return measurement
+
+    # run multichannel experiment with csv writer
+    async with pypalmsens.InstrumentPoolAsync(instruments) as pool:
+        tasks = await pool.submit(my_task, method=method)
+        results = await asyncio.gather(*tasks)
+
+    print(results)
+
+    # run multichannel experiment with csv writer
+    async with pypalmsens.InstrumentPoolAsync(instruments) as pool:
+        tasks = await pool.measure_hw_sync(method, main_channel=0)
+        results = await asyncio.gather(*tasks)
+
+    print(results)
+
+    # callback
+    # hardware sync
+    # custom loop
 
 
 asyncio.run(main())
--- python/src/pypalmsens/_instruments/instrument_pool.py
+++ python/src/pypalmsens/_instruments/instrument_pool.py
@@ -1,15 +1,24 @@
 from __future__ import annotations
 
 import asyncio
+from typing import Any, Callable, Protocol
 
 from .._methods import MethodSettings
 from ._common import Instrument
 from .instrument_manager_async import InstrumentManagerAsync
 
+# from typing_extensions import Protocol  # if you're using Python 3.6
+
+
+class CustomFunc(Protocol):
+    def __call__(self, manager: InstrumentManagerAsync, **kwargs: Any): ...
+
 
 class InstrumentPoolAsync:
-    def __init__(self, devices: list[Instrument]):
-        self.managers = [InstrumentManagerAsync(device) for device in devices]
+    def __init__(self, devices: list[Instrument], callback: None | Callable = None):
+        self.managers = [
+            InstrumentManagerAsync(device, callback=callback) for device in devices
+        ]
 
     async def __aenter__(self):
         await self.connect()
@@ -43,4 +52,50 @@ class InstrumentPoolAsync:
         tasks = []
         for manager in self.managers:
             tasks.append(manager.measure(method))
+        return tasks
+
+    async def submit(self, func: CustomFunc, **kwargs: Any):
+        """Concurrently start measurement on all managers in the pool.
+
+        Parameters
+        ----------
+        func : Callable
+            This function gets called with an instance of
+            `InstrumentManagerAsync` as the argument.
+        **kwargs
+            These keyword arguments are passed on to the submitted functio.
+        """
+        print(kwargs)
+        tasks = []
+        for manager in self.managers:
+            tasks.append(func(manager, **kwargs))
+        return tasks
+
+    async def measure_hw_sync(self, method: MethodSettings, *, main_channel: int = 0):
+        """Concurrently start measurement on all managers in the pool.
+
+        Parameters
+        ----------
+        method : MethodSettings
+            Method parameters for measurement.
+        main_channel : int
+            Index of the main channel for hardware sync
+        """
+        follower_sync_tasks = []
+        tasks = []
+
+        hw_sync_manager = self.managers[main_channel]
+
+        for i, manager in enumerate(self.managers):
+            if i == main_channel:
+                continue
+
+            initiated, result = manager.initiate_hardware_sync_follower_channel(method)
+            follower_sync_tasks.append(initiated)
+            tasks.append(result)
+
+        await asyncio.gather(*follower_sync_tasks)
+
+        tasks.append(hw_sync_manager.measure(method))
+
         return tasks
