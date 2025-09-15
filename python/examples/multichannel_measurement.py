6ec82c627f252f65273b5167259c183cd65f4def
Stef Smeets <stef@palmsens.com>
2025-09-15 15:12:05 +0200

    Refactor callback

--- python/examples/multichannel_measurement.py
+++ python/examples/multichannel_measurement.py
@@ -12,6 +12,23 @@ def stream_to_csv_callback(new_data, csv_writer):
         csv_writer.writerow([point['index'], point['x'], point['y']])
 
 
+async def stream_to_csv(manager: InstrumentManagerAsync, *, method: MethodSettings):
+    """Measure with a custom csv writer callback."""
+    serial = await manager.get_instrument_serial()
+
+    with open(f'{serial}.csv', 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=' ')
+
+        callback = functools.partial(stream_to_csv_callback, csv_writer=csv_writer)
+        manager.callback = callback
+
+        measurement = await manager.measure(method)
+
+    print(f'Wrote data to {csv_file.name}')
+
+    return measurement
+
+
 async def main():
     method = pypalmsens.ChronoAmperometry(
         interval_time=0.004,
@@ -21,21 +38,7 @@ async def main():
 
     instruments = await pypalmsens.discover_async(ftdi=True)
 
-    # run multichannel experiment with custom task
-    async def stream_to_csv(manager: InstrumentManagerAsync, *, method: MethodSettings):
-        serial = await manager.get_instrument_serial()
-
-        with open(f'{serial}.csv', 'w', newline='') as csv_file:
-            csv_writer = csv.writer(csv_file, delimiter=' ')
-
-            callback = functools.partial(stream_to_csv_callback, csv_writer=csv_writer)
-            manager.callback = callback
-
-            measurement = await manager.measure(method)
-
-        print(f'Wrote data to {csv_file.name}')
-
-        return measurement
+    print(instruments)
 
     # run multichannel experiment with csv writer
     async with pypalmsens.InstrumentPoolAsync(instruments) as pool:
