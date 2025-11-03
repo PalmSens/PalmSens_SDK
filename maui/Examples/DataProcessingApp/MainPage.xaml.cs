2a7f7310d741a88c0bb80cc999b6124e7179e41f
Stef Smeets <stef@palmsens.com>
2025-11-03 17:04:13 +0100

    Fix Data processing example

--- maui/Examples/DataProcessingApp/MainPage.xaml.cs
+++ maui/Examples/DataProcessingApp/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 using OxyPlot;
 using PalmSens;
+using PalmSens.Core.Simplified;
 using PalmSens.Core.Simplified.Data;
 using PalmSens.Data;
 using SDKPlot;
@@ -33,22 +34,17 @@ namespace DataProcessingExample
 
         private ObservableCollection<string> _log = [];
 
-        private void LoadData(object? sender, EventArgs e)
+        private async void LoadData(object? sender, EventArgs e)
         {
             LoadDataBtn.IsEnabled = false;
             Log.Add($"Loading example data...");
 
             SessionManager session = null;
 
-            // TODO: Use Platform agnostic function
-#if WINDOWS
-            session = LoadSaveHelperFunctions.LoadSessionFile("Example.pssession");
-#else
-            session = [];
-#endif
+            using var stream = await FileSystem.OpenAppPackageFileAsync("Example.pssession");
+            using var reader = new StreamReader(stream);
 
-            foreach (Measurement measurement in session)
-                _measurements.Add(new SimpleMeasurement(measurement));
+            _measurements = SimpleLoadSaveFunctions.LoadMeasurements(reader);
 
             SimpleMeasurement dpvExample = _measurements.FirstOrDefault(meas => meas.Title == "DPV Example");
             _activeMeasurement = dpvExample;
