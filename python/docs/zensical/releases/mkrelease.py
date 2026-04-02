3b6f1950312c0df86aacae5edcc0f9d763cee4b9
Stef Smeets <stefsmeets@users.noreply.github.com>
2026-04-02 12:25:54 +0200

    Add releases page

--- python/docs/zensical/releases/mkrelease.py
+++ python/docs/zensical/releases/mkrelease.py
@@ -1,33 +1,62 @@
 from __future__ import annotations
 
+import datetime
 import json
-
-name = 'python'
-version = '1.6.0'
-tag = f'{name}-{version}'
-
-with open(f'{tag}.json') as f:
-    data = json.load(f)
-
-TEMPLATE = """
-## {name}
-
-{tagName} - {url}
-{publishedAt}
-pip: https://pypi.org/project/pypalmsens-{version}'
-
-{body}
-"""
-
-with open(f'{tag}.md', 'w') as f:
-    string = TEMPLATE.format(version=version, **data)
-    print(string)
-    f.write(string)
-
-# title
-# released
-# body
-# pip link
+import subprocess as sp
+import sys
+from textwrap import dedent
 
 
-breakpoint()
+def get_releases(name: str = 'python') -> list[str]:
+    cmd = 'gh release list'.split()
+    p = sp.run(cmd, capture_output=True)
+    lines = p.stdout.decode().splitlines()
+
+    releases = []
+
+    for line in lines:
+        tag = line.split('\t')[2]
+        if tag.startswith(name):
+            releases.append(tag)
+
+    return releases
+
+
+releases = sys.argv[1:]
+
+if not releases:
+    releases = get_releases()
+
+for release in releases:
+    name, version = release.split('-')
+
+    try:
+        with open(f'{release}.json') as f:
+            data = json.load(f)
+    except IOError:
+        cmd = f'gh release view {release} --json name,body,url,tagName,publishedAt'.split()
+        p = sp.run(cmd, capture_output=True)
+        payload = p.stdout.decode()
+
+        with open(f'{release}.json', 'w') as f:
+            f.write(payload)
+
+        data = json.loads(payload)
+
+    time = datetime.datetime.fromisoformat(data['publishedAt'])
+
+    TEMPLATE = dedent("""\
+    # {name}
+
+    > :fontawesome-brands-github: <a href="{url}">{tagName}</a>
+    - :fontawesome-brands-python: <a href="https://pypi.org/project/pypalmsens-{version}">pypalmsens-{version}</a>
+    - :fontawesome-solid-calendar: {time}
+
+    {body}
+    """)
+
+    with open(f'{release}.md', 'w') as f:
+        string = TEMPLATE.format(version=version, time=time.date(), **data)
+        f.write(string)
+
+    print(release)
