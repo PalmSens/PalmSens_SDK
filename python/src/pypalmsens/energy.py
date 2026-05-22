35e655fc956315f25286589a2e8d0190b1b545c7
Stef Smeets <stef@palmsens.com>
2026-05-22 12:15:53 +0200

    Add BatteryCycling technique

---
+++ python/src/pypalmsens/energy.py
@@ -0,0 +1,11 @@
+"""Define classes for energy measurements."""
+
+from __future__ import annotations
+
+from ._methods.energy import (
+    BatteryCycling,
+)
+
+__all__ = [
+    'BatteryCycling',
+]
