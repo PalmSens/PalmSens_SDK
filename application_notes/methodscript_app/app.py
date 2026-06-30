e8dd1d8d6cdc7ecae56510cd3e3d756d79a4c367
Stef Smeets <stef@palmsens.com>
2026-06-30 10:38:06 +0200

    Remove old app and fix new one

--- application_notes/methodscript_app/app.py
+++ application_notes/methodscript_app/app.py
@@ -2,110 +2,154 @@
 # requires-python = ">=3.11"
 # dependencies = [
 #     "pypalmsens>=1.9",
-#     "pandas>=2.3",
-#     "plotly>=6.6",
 #     "streamlit>=1.55",
 # ]
 # ///
 
 from __future__ import annotations
 
-import plotly.express as px
 import streamlit as st
-from pydantic import BaseModel
 
 import pypalmsens as ps
 from pypalmsens.data import CallbackData
 
 st.set_page_config(
-    page_title='Measurement dashboard',
-    page_icon=':microscope:',
+    page_title='Battery Cycling',
+    page_icon=':material/battery_android_frame_bolt:',
     menu_items={
         'Get Help': 'https://palmsens.com/contact',
         'Report a bug': 'https://github.com/palmsens/palmsens_sdk/issues',
     },
+    layout='wide',
+    initial_sidebar_state='expanded',
 )
-
-TEMPLATE = """\
-e
-wait {wait_time}m
-if 1 < 2
-    send_string "Hello {name}"
-endif
-
-"""
 
 connect_to_device = st.cache_resource(ps.connect)
 
 
-class MyVariables(BaseModel):
-    wait_time: int
-    """Time to wait."""
-
-    name: str = 'World'
-    """Who to greet."""
-
-
 def main():
-    st.title('Cyclic Voltammetry')
+    st.title('Battery Cycling')
 
-    with st.form('parameters_form'):
-        equilibration_time = st.number_input(
-            'Equilibration time', value=0.0, help='Equilibration time in s.'
-        )
-        begin_potential = st.number_input(
-            'Begin potential',
-            value=-0.5,
-            help='Potential where the scan starts and stops at in V.',
-        )
-        vertex1_potential = st.number_input(
-            'Vertex 1 potential',
-            value=0.5,
-            help='First potential where direction reverses in V.',
-        )
-        vertex2_potential = st.number_input(
-            'Vertex 2 potential',
-            value=-0.5,
-            help='Second potential where direction reverses. V.',
-        )
-        step_potential = st.number_input(
-            'Step potential', value=0.1, help='Potential step size in V.'
-        )
-        scanrate = st.number_input('Scanrate', value=1.0, help='Scan rate in V/s.')
+    c1, c2 = st.columns(2)
 
-        submitted = st.form_submit_button('Start measurement')
-        if not submitted:
-            st.stop()
+    with st.sidebar:
+        potential_max = st.number_input(
+            'Potential Max (mV)',
+            value=4300,
+            help='Maximum potential to charge to (units: mV).',
+        )
 
-    method = ps.CyclicVoltammetry(
-        equilibration_time=equilibration_time,
-        begin_potential=begin_potential,
-        vertex1_potential=vertex1_potential,
-        vertex2_potential=vertex2_potential,
-        step_potential=step_potential,
-        scanrate=scanrate,
+        current_min = st.number_input(
+            'Current Min (μA)',
+            value=5,
+            help='Minimum current to stop the CV charge step (units: μA).',
+        )
+
+        potential_min = st.number_input(
+            'Potential Min (mV)',
+            value=2500,
+            help='Minimum potential to discharge to (units: mV).',
+        )
+
+        current_charge = st.number_input(
+            'Current Charge (μA)',
+            value=100,
+            help='Constant current to charge with (units: μA).',
+        )
+
+        current_discharge = st.number_input(
+            'Current Discharge (μA)',
+            value=-100,
+            help='Constant current to discharge with (units: μA).',
+        )
+
+        cycles = st.number_input(
+            'Cycles', value=1, help='Number of charge and discharge cycles.'
+        )
+
+        interval = st.number_input(
+            'Interval (s)',
+            value=10,
+            help='Interval time of each measurement point (units: s).',
+        )
+
+        max_time = st.number_input(
+            'Max Time (s)',
+            value=3,
+            help='Maximum duration of each step (if the cut-off is not met) (units: s).',
+        )
+
+        delta_v = st.number_input(
+            'Delta V (μV)',
+            value=100,
+            min_value=0,
+            help='Minimum potential variation required for plotting data in CC steps (units: μV).',
+        )
+
+        delta_i = st.number_input(
+            'Delta I (nA)',
+            value=500,
+            min_value=0,
+            help='Minimum current variation reuqired for plotting data in the CV step (units: nA).',
+        )
+
+        delta_t = st.number_input(
+            'Delta T (ms)',
+            value=100,
+            help='Maximum time without plotting data (units: ms).',
+        )
+
+    method = ps.energy.experimental_BatteryCycling(
+        potential_max=potential_max,
+        current_min=current_min,
+        potential_min=potential_min,
+        current_charge=current_charge,
+        current_discharge=current_discharge,
+        cycles=cycles,
+        interval=interval,
+        max_time=max_time,
+        delta_v=delta_v,
+        delta_i=delta_i,
+        delta_t=delta_t,
     )
+
+    ms = method.to_methodscript()
+
+    with st.expander('Click to show generated MethodSCRIPT'):
+        st.code(ms.script, line_numbers=True, height=600)
+
+    c1, c2 = st.columns(2)
+    with c1:
+        st.download_button(
+            'Save as script (.mscr)',
+            data=ms.script,
+            file_name='battery_cycler.mscr',
+            mime='text/plain',
+            icon=':material/download:',
+            width='stretch',
+        )
+    with c2:
+        _ = st.download_button(
+            'Save as method file (.psmethod)',
+            data=ms._serialize(),
+            file_name='battery_cycler.psmethod',
+            mime='text/plain',
+            icon=':material/download:',
+            width='stretch',
+        )
 
     manager: ps.InstrumentManager = connect_to_device()
     assert manager.is_connected()
 
-    chart = st.empty()
-
     def update(data: CallbackData):
-        fig = px.line(
-            x=data.x_array,
-            y=data.y_array,
-            title='CV',
+        status.update(
+            label=f'New data: x={data.last_x:.2f} y={data.last_y:.2f}',
         )
-        fig.update_layout(
-            xaxis={'title': {'text': 'Current / mA'}},
-            yaxis={'title': {'text': 'Potential / V'}},
-        )
-        chart.plotly_chart(fig)
 
-    manager.measure(method, callback=update)
-
-    st.write('Done')
+    with st.status('Starting measurement') as status:
+        status.update(label='Collecting data')
+        manager.measure(method, callback=update)
+        status.update(label='Measurement finished!', state='complete')
 
 
 if __name__ == '__main__':
